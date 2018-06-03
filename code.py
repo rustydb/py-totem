6f5399965e9bf212d57064ebf6d3db0fcad01541
Russell Bunch <rusty@4lambda.io>
2018-06-03 15:43:37 -0500

    Added docstrings and basic run loop to hit all functions.

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
 import time
+
 import board
+
 import neopixel
 
 PIXELS_PIN = board.A1
-PIXELS_NUM = 1
+PIXELS_NUM = 72
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
@@ -12,13 +14,18 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 
 onboard = neopixel.NeoPixel(board.NEOPIXEL, 1)
-onboard.brightness = 1.0
-pixels = neopixel.NeoPixel(PIXELS_PIN, PIXELS_NUM, brightness=0.3, auto_write=False)
+onboard.brightness = 0.1
+pixels = neopixel.NeoPixel(PIXELS_PIN, PIXELS_NUM, brightness=0.7, auto_write=False)
 
 
 def wheel(pos: int) -> tuple:
-    # Input a value 0 to 255 to get a color value.
-    # The colors are a transition r - g - b - back to r.
+    """
+    Converts an integer from 0 - 255 to an RGB color value. The colors are a transition
+    r - g - b - back to r.
+    :param pos: Position on the color wheel (0 - 255).
+    :returns: RGB Tuple.
+    """
+    # If out of range, return black.
     if pos < 0 or pos > 255:
         return 0, 0, 0
     if pos < 85:
@@ -30,30 +37,54 @@ def wheel(pos: int) -> tuple:
     return pos * 3, 0, 255 - pos * 3
 
 
-def color_chase(color: int, wait: float) -> None:
+def color_chase(color: tuple, wait: float) -> None:
+    """
+    Lights each LED to the given color one at a time.
+    :param color: RGB color tuple to chase with.
+    :param wait: Time before lighting next LED.
+    """
+
+    # Loop through each pixel.
     for i in range(PIXELS_NUM):
         pixels[i] = color
         time.sleep(wait)
         pixels.show()
+
+    # Wait before any other function call so full strand can be lit.
     time.sleep(0.5)
 
 
 def rainbow_cycle(wait: float) -> None:
+    """
+    Makes a rainbow over PIXELS_NUM.
+    :param wait: The time to cycle for the rainbow.
+    """
+
+    # Loop through each color (0 - 255).
     for j in range(255):
+
+        # Loop through each LED.
         for i in range(PIXELS_NUM):
+
+            # LED position * number of colors floor divided by number of pixels plus current color.
             rc_index = (i * 256 // PIXELS_NUM) + j
+
+            # Set pixel RGB tuple set by wheel()
             pixels[i] = wheel(rc_index & 255)
+
+        # Light up the LEDs and wait until next color.
         pixels.show()
         time.sleep(wait)
 
 
+# Main run loop.
 while True:
-    onboard.fill(PURPLE)
+    onboard.fill(GREEN)
     onboard.show()
-    time.sleep(1)
-    onboard.fill(CYAN)
-    onboard.show()
-    time.sleep(1)
+    color_chase(PURPLE, 0.1)
     onboard.fill(RED)
     onboard.show()
-    time.sleep(1)
+    color_chase(BLUE, 0.1)
+    onboard.fill(BLUE)
+    onboard.show()
+    rainbow_cycle(0.1)
