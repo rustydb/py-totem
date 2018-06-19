d4b637ef2cb52727a6f589e2370fb819a632e22a
Russell Bunch <rusty@4lambda.io>
2018-06-19 00:08:52 -0500

    Added final touches
    Added an "orange" color. Added a voltage indicator with a scale from 3.2
    to 3.7+ volts. Setup an animation of color chasing, rainbow, color
    chasing.

--- code.py
+++ code.py
@@ -2,20 +2,66 @@ import time
 
 import board
 
-import neopixel
-
+from neopixel import NeoPixel
+     
+from analogio import AnalogIn
 PIXELS_PIN = board.A1
-PIXELS_NUM = 72
+PIXELS_NUM = 71
 RED = (255, 0, 0)
+ORANGE = (255, 20, 0)
 YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 
-onboard = neopixel.NeoPixel(board.NEOPIXEL, 1)
+onboard = NeoPixel(board.NEOPIXEL, 1)
 onboard.brightness = 0.1
-pixels = neopixel.NeoPixel(PIXELS_PIN, PIXELS_NUM, brightness=0.7, auto_write=False)
+pixels = NeoPixel(PIXELS_PIN, PIXELS_NUM, brightness=0.1)
+vbat_voltage = AnalogIn(board.D9)
+
+
+def color_chase(color: tuple, wait: float) -> None:
+    """
+    Lights each LED to the given color one at a time.
+    :param color: RGB color tuple to chase with.
+    :param wait: Time before lighting next LED.
+    """
+    for i in range(PIXELS_NUM):
+        pixels[i] = color
+        time.sleep(wait)
+
+
+def set_voltage_led() -> None:
+    """
+    Sets the LED to a color corresponding to roughly: "full charge",
+    "good charge", "fair charge", and "low".
+    """
+    battery_voltage = (vbat_voltage.value * 3.3) / 65536 * 2
+    if battery_voltage >= 3.7:
+        onboard.fill(GREEN)
+    elif 3.5 <= battery_voltage < 3.7:
+        onboard.fill(YELLOW)
+    elif 3.3 <= battery_voltage < 3.5:
+        onboard.fill(ORANGE)
+    elif battery_voltage < 3.3:
+        onboard.fill(RED)
+    else:
+        onboard.fill(CYAN)
+    onboard.show()
+
+
+def rainbow_cycle() -> None:
+    """
+    Makes a rainbow over PIXELS_NUM.
+    """
+    for i in range(PIXELS_NUM):
+
+        # LED position * number of colors floor divided by number of pixels plus current color.
+        rc_index = (i * 256 // PIXELS_NUM) + 1
+
+        # Set pixel RGB tuple set by wheel()
+        pixels[i] = wheel(rc_index & 255)
 
 
 def wheel(pos: int) -> tuple:
@@ -37,54 +83,13 @@ def wheel(pos: int) -> tuple:
     return pos * 3, 0, 255 - pos * 3
 
 
-def color_chase(color: tuple, wait: float) -> None:
-    """
-    Lights each LED to the given color one at a time.
-    :param color: RGB color tuple to chase with.
-    :param wait: Time before lighting next LED.
-    """
-
-    # Loop through each pixel.
-    for i in range(PIXELS_NUM):
-        pixels[i] = color
-        time.sleep(wait)
-        pixels.show()
-
-    # Wait before any other function call so full strand can be lit.
-    time.sleep(0.5)
-
-
-def rainbow_cycle(wait: float) -> None:
-    """
-    Makes a rainbow over PIXELS_NUM.
-    :param wait: The time to cycle for the rainbow.
-    """
-
-    # Loop through each color (0 - 255).
-    for j in range(255):
-
-        # Loop through each LED.
-        for i in range(PIXELS_NUM):
-
-            # LED position * number of colors floor divided by number of pixels plus current color.
-            rc_index = (i * 256 // PIXELS_NUM) + j
-
-            # Set pixel RGB tuple set by wheel()
-            pixels[i] = wheel(rc_index & 255)
-
-        # Light up the LEDs and wait until next color.
-        pixels.show()
-        time.sleep(wait)
-
-
 # Main run loop.
 while True:
-    onboard.fill(GREEN)
-    onboard.show()
-    color_chase(PURPLE, 0.1)
-    onboard.fill(RED)
-    onboard.show()
-    color_chase(BLUE, 0.1)
-    onboard.fill(BLUE)
-    onboard.show()
-    rainbow_cycle(0.1)
+
+    # Red is the "cheapest visible" color for "power-on" LED.
+    set_voltage_led()
+    for color in [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]:
+        set_voltage_led()
+        color_chase(color, 0.01)
+        set_voltage_led()
+        rainbow_cycle()
